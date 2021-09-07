2e6e625c98ae42964abd5ce577b632656a2f8ce3
NerajM <35711461+NerajM@users.noreply.github.com>
2021-09-07 19:31:13 -0400

    Added Hero Notification Box (#8)
    * Added Hero Notification Box
    
    * Addressing PR comments
    
    * Fix not required props
    
    Co-authored-by: Shawn Hu <shawnhu97@gmail.com>
    
    * Fixed lint issues, added default props, and minor css tweaks
    
    * Fix typing issue for props
    
    Co-authored-by: Shawn Hu <shawnhu97@gmail.com>
    
    * disabled linting rule
    
    * merged master
    
    Co-authored-by: Shawn Hu <shawnhu97@gmail.com>

---
+++ components/HeroNotificationBox.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import Image from 'next/image';
+import Arrow from '../public/arrow_forward.svg';
+
+type HeroNotificationBoxProps = {
+  title?: string;
+  content?: string;
+};
+
+const HeroNotificationBox: FC<HeroNotificationBoxProps> = ({
+  title,
+  content,
+}) => (
+  <div className="heroNotificationBox">
+    <p className="text-2xl font-bold">{title}</p>
+    <div className="flex justify-between items-center">
+      <p className="text-base overflow-clip flex-shrink font-medium">
+        {content}
+      </p>
+      <button type="button" className="flex-none max-h-12">
+        <Image src={Arrow} alt="Events" />
+      </button>
+    </div>
+  </div>
+);
+
+HeroNotificationBox.defaultProps = {
+  title: 'We’re hosting an event!',
+  content: 'This is going to be a fun event!',
+};
+
+export default HeroNotificationBox;
