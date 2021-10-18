259d6bf62076f7481f156b6a277c644924134790
Shawn Hu <shawnhu97@gmail.com>
2021-10-18 17:46:35 -0400

    Feat/home event section (#24)
    * working on add events
    
    * added event section
    
    * added button
    
    * removed useless styling
    
    * added key

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,44 @@ import { FC } from 'react';
 import Card from '@components/Card';
 import NavBar from '@components/NavBar';
 import ScrollingAvatars from '@components/ScrollingAvatars/ScrollingAvatarsContainer';
-// import Icon from '@components/Icon';
+import EventSection from '@components/EventSection';
 
 const Home: FC = () => (
   <div className="h-screen">
+    <EventSection
+      eventCardInfos={[
+        {
+          imgSrc: '/chef.jpg',
+          date: 'Sept 25',
+          author: 'Brian Li',
+          description: 'Masterchef Series: TA vs Professors',
+        },
+        {
+          imgSrc: '/chef.jpg',
+          date: 'Sept 26',
+          author: 'Brian Li',
+          description: 'Masterchef Series: TA vs Professors',
+        },
+        {
+          imgSrc: '/chef.jpg',
+          date: 'Sept 27',
+          author: 'Brian Li',
+          description: 'Masterchef Series: TA vs Professors',
+        },
+        {
+          imgSrc: '/chef.jpg',
+          date: 'Sept 28',
+          author: 'Brian Li',
+          description: 'Masterchef Series: TA vs Professors',
+        },
+      ]}
+    />
     <NavBar />
     <Card className="m-10 h-2/3 flex p-20 bg-gradient-to-b from-blue-600 to-white">
       <div className="flex-1 p-10 text-white items-center">
         <h1 className="text-5xl py-5">Welcome to MCSS!</h1>
-        We exist to support Math and Computer Science students in everything you
-        do. From info, to advocacy, to events, the MCSS exists so that you can
-        change the future.
+        We exist to support Math and Computer Science students in everything you do. From info, to
+        advocacy, to events, the MCSS exists so that you can change the future.
       </div>
       <div className="flex-1 p-10">This is some other content</div>
     </Card>
