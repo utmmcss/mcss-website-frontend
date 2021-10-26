85b13fcaa7668a92735bd4043118e01516e24b60
Shawn Hu <shawnhu97@gmail.com>
2021-10-26 18:25:35 -0400

    adding some more sections

---
+++ components/Sections/Home/WhatWeDoSection.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import SectionWrapper from '@components/Common/SectionWrapper';
+
+const WhatWeDoSection: FC = () => (
+  <SectionWrapper title="WHAT WE DO" subtitle="SYSTEM">
+    <div>he</div>
+  </SectionWrapper>
+);
+
+export default WhatWeDoSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,17 @@ import EventSection from '@components/Sections/Home/EventSection';
 import HomeIntro from '@components/Sections/Home/HomeIntro';
 import AboutUs from '@components/Sections/Home/AboutUs';
 import JoinUsSection from '@components/Sections/Home/JoinUsSections';
+import WhatWeDoSection from '@components/Sections/Home/WhatWeDoSection';
 
 const Home: FC = () => (
   <div>
     <HomeIntro />
     <AboutUs />
+    <WhatWeDoSection />
     <TeamMemberSection />
     <iframe
       width="100%"
-      height="600"
+      height="550"
       src="https://www.youtube.com/embed/_pZ0IyExtQ4"
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
