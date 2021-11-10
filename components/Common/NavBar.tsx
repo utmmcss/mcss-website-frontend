d54f07f2e5294671e1fcc4f6b761845c82662bb3
Shrey Vakil <shrey.vakil@mail.utoronto.ca>
2021-11-09 21:34:20 -0500

    Added search to the events page

--- components/Common/NavBar.tsx
+++ components/Common/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import Logo from '@public/mcssLogo.svg';
+import SearchIcon from '@mui/icons-material/Search';
 
 const NavBar: FC = () => {
   const [active, setActive] = useState(false);
@@ -22,6 +23,13 @@ const NavBar: FC = () => {
       <nav className="flex items-center justify-between flex-wrap mt-8 mx-14">
         <div className="flex item-center flex-shrink-0 cursor-pointer">
           <Image src={Logo} alt="MCSS logo" height={80} onClick={() => router.push('/')} />
+        </div>
+        <div className={`${
+          (router.pathname === '/Events') ? '' : 'invisible'
+        }  m-5 w-full h-12 block flex-grow lg:flex lg:items-center lg:w-auto `}
+        >
+          <div className="bg-gray-200 h-full text-base text-gray-400 pl-3 py-3"><SearchIcon className="  " /></div>
+          <input className="bg-gray-200 px-5 pr-16 rounded-sm text-base text-gray-400 w-full h-full focus:outline-none" type="search" name="" id="" placeholder="Search Events" />
         </div>
         <button className="lg:hidden" id="open-menu" onClick={handleClick} type="button">
           <svg
