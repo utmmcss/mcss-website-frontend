c8f1f57a662b5b634bc42bdc3821ad945fde72e9
NerajM <35711461+NerajM@users.noreply.github.com>
2021-12-14 15:55:31 -0500

    Added key as a prop to the general events section

--- components/Common/GeneralEvents.tsx
+++ components/Common/GeneralEvents.tsx
@@ -18,10 +18,10 @@ type EventCardInfosTypes = {
 const GeneralEvents: FC<GeneralEventsProps> = ({ eventCardInfos }) => (
   <div className="event-section md:m-0 lg:mx-20 2xl:mx-40">
     <div className="flex flex-wrap mt-10">
-      {eventCardInfos.map(({ imgSrc, date, author, description }) => (
+      {eventCardInfos.map(({ imgSrc, date, author, description, key }) => (
         <div
           className="w-full sm:w-1/3 md:w-1/4 px-6 py-4"
-          key={`${date}${author}${description}`}
+          key={key}
         >
           <Card className="card rounded-lg relative">
             <div className="tag">Food</div>
@@ -57,48 +57,56 @@ GeneralEvents.defaultProps = {
       date: 'Sept 25',
       author: 'Brian Li',
       description: 'Masterchef Series: TA vs Professors',
+      key: '1',
     },
     {
       imgSrc: '/chef.jpg',
       date: 'Sept 26',
       author: 'Brian Li',
       description: 'Masterchef Series: TA vs Professors',
+      key: '2',
     },
     {
       imgSrc: '/chef.jpg',
       date: 'Sept 27',
       author: 'Brian Li',
       description: 'Masterchef Series: TA vs Professors',
+      key: '3',
     },
     {
       imgSrc: '/chef.jpg',
       date: 'Sept 28',
       author: 'Brian Li',
       description: 'Masterchef Series: TA vs Professors',
+      key: '4',
     },
     {
       imgSrc: '/chef.jpg',
       date: 'Sept 28',
       author: 'Brian Li',
       description: 'Masterchef Series: TA vs Professors',
+      key: '5',
     },
     {
       imgSrc: '/chef.jpg',
       date: 'Sept 28',
       author: 'Brian Li',
       description: 'Masterchef Series: TA vs Professors',
+      key: '6',
     },
     {
       imgSrc: '/chef.jpg',
       date: 'Sept 28',
       author: 'Brian Li',
       description: 'Masterchef Series: TA vs Professors',
+      key: '7',
     },
     {
       imgSrc: '/chef.jpg',
       date: 'Sept 28',
       author: 'Brian Li',
       description: 'Masterchef Series: TA vs Professors',
+      key: '8',
     },
   ],
 };
