49984445e73de9812b89975dd5ba8d27487480fd
TheFatPanda97 <shwnhu97@gmail.com>
2021-12-24 23:06:01 -0500

    updated search bar

--- components/Common/NavBar.tsx
+++ components/Common/NavBar.tsx
@@ -1,20 +1,56 @@
-import React, { FC } from 'react';
+import { FC, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
 import SearchIcon from '@mui/icons-material/Search';
+import Select, { components, DropdownIndicatorProps } from 'react-select';
 
 import Logo from '@public/mcssLogo.svg';
 import MediaQueryContainer from './MediaQueryContainer';
 
+const DropdownIndicator: FC<DropdownIndicatorProps<{ value: string; label: string }, false>> = (
+  props,
+) => {
+  return (
+    <components.DropdownIndicator {...props}>
+      <SearchIcon />
+    </components.DropdownIndicator>
+  );
+};
+
 const NavBar: FC = () => {
+  const [value, setValue] = useState<{ value: string; label: string } | null>();
   const router = useRouter();
   const links = [
     { label: 'Home', href: '/' },
     { label: 'Events', href: '/Events' },
     { label: 'Blogs', href: '/Blogs' },
   ];
+  const options = [
+    {
+      value: 'chocolatechocolatechocolatechocolatechocolatechocolatechocolatechocolate',
+      label: 'chocolatechocolatechocolatechocolatechocolatechocolatechocolatechocolate',
+    },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
+  const customStyles = {
+    control: (provided: any) => ({
+      ...provided,
+      height: '100%',
+    }),
+    indicatorSeparator: () => ({
+      display: 'hidden',
+    }),
+    option: (provided: any) => ({
+      ...provided,
+      textOverflow: 'ellipsis',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+    }),
+  };
 
   return (
     <nav className="flex items-center justify-between flex-wrap mt-8 mx-6 md:mx-14">
@@ -27,15 +63,23 @@ const NavBar: FC = () => {
           hidden: router.pathname !== '/Events',
         })}
       >
-        <div className="bg-gray-200 h-full text-base text-gray-400 pl-3 py-3">
-          <SearchIcon className="  " />
-        </div>
-        <input
-          className="bg-gray-200 px-5 rounded-sm text-base text-gray-400 w-full md:max-w-sm h-full focus:outline-none"
-          type="search"
-          name=""
-          id=""
-          placeholder="Search Events"
+        <Select
+          className="w-full md:max-w-sm h-full"
+          styles={customStyles}
+          value={value}
+          onChange={(selectedOption) => setValue(selectedOption)}
+          options={options}
+          components={{ DropdownIndicator }}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: 0,
+            colors: {
+              ...theme.colors,
+              primary25: 'rgb(243 232 255)',
+              primary: '#7C3AED',
+            },
+          })}
+          maxMenuHeight={130}
         />
       </div>
       <MediaQueryContainer showOnMobile>
