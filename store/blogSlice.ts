b7956779cee282494113c7d188269d831dab6677
Shawn Hu <shawnhu97@gmail.com>
2021-12-28 22:16:31 -0500

    added blogs slice

---
+++ store/blogSlice.ts
@@ -0,0 +1,119 @@
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import _ from 'underscore';
+
+import type { DataAttribute, DataAttributes } from '@store/storeTypes';
+import { getAPI } from '@utils/helper';
+import type { RootState } from './store';
+
+// Define a type for the slice state
+interface Blog {
+  title: string;
+  creator: string;
+  updatedDatetime: string;
+  coverImageUrl: string;
+  content: string;
+  categories: string[];
+  featured: boolean;
+}
+interface BlogState {
+  blogs: Record<number, Blog>;
+  categories: string[];
+}
+
+export const getAllCategories = createAsyncThunk<
+  string[],
+  /** no args for this async dispatch */
+  void,
+  {
+    state: RootState;
+  }
+>('blogs/fetchAllCategories', async () => {
+  interface CategoryResponse {
+    type: string;
+  }
+
+  const response: DataAttributes<CategoryResponse> = await getAPI('/blog-categories');
+  let parsedCategories: string[] = [];
+
+  if (response?.data) {
+    parsedCategories = response.data.map(({ attributes }) => attributes.type);
+  }
+
+  return parsedCategories;
+});
+
+export const getAllBlogs = createAsyncThunk<
+  Record<number, Blog>,
+  /** no args for this async dispatch */
+  void,
+  {
+    state: RootState;
+  }
+>('blogs/fetchAllBlogs', async () => {
+  interface BlogResponse extends Omit<Blog, 'categories'> {
+    updatedAt: string;
+    categories: DataAttributes<{ type: string }>;
+    cover_image: DataAttribute<{ url: string }>;
+  }
+
+  interface APIResponse {
+    data: {
+      id: number;
+      attributes: BlogResponse;
+    }[];
+  }
+
+  const response: APIResponse = await getAPI('/blogs?populate=*');
+  const parsedBlogs: Record<number, Blog> = {};
+
+  if (response?.data) {
+    response.data.forEach(
+      ({
+        id,
+        attributes: { title, creator, cover_image, content, categories, featured, updatedAt },
+      }) => {
+        const parsedCategories = !_.isEmpty(categories)
+          ? categories.data.map(({ attributes: { type } }) => type)
+          : ['Other'];
+
+        parsedBlogs[id] = {
+          title,
+          creator,
+          coverImageUrl: `${process.env.NEXT_PUBLIC_API_URL}${cover_image.data.attributes.url}`,
+          content: content.replaceAll('/uploads/', `${process.env.NEXT_PUBLIC_API_URL}/uploads/`),
+          categories: parsedCategories,
+          featured,
+          updatedDatetime: updatedAt,
+        };
+      },
+    );
+  }
+  return parsedBlogs;
+});
+
+// Define the initial state using that type
+const initialState: BlogState = {
+  blogs: [],
+  categories: [],
+};
+
+const blogSlice = createSlice({
+  name: 'blogs',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // load all blogs
+    builder.addCase(getAllBlogs.fulfilled, (state, action) => {
+      state.blogs = action.payload;
+    });
+
+    // load all blog categories
+    builder.addCase(getAllCategories.fulfilled, (state, action) => {
+      state.categories = action.payload;
+    });
+  },
+});
+
+export default blogSlice.reducer;
