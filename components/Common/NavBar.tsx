8744fdd1d44a3205a46693c4ed75abc8d6a91bba
eruditeme <evangelineyiqin@gmail.com>
2023-11-02 03:10:15 -0400

    corrected indent on navbar

--- components/Common/NavBar.tsx
+++ components/Common/NavBar.tsx
@@ -47,7 +47,7 @@ const NavBar: FC = () => {
     { label: 'Blogs', href: '/Blogs' },
     { label: 'Sponsors', href: '/Sponsors' },
   ];
-  const searchBarWhiteList = ['/Events', '/Blogs', '/Sponsors'];
+  const searchBarWhiteList = ['/Events', '/Blogs', '/Sponsors', '/Academics'];
   const partialRouteMatch = searchBarWhiteList.some((route) => router.pathname.includes(route));
   const options = [
     ...Object.entries(events).map(([id, { title }]) => ({ label: `Event: ${title}`, value: id })),
@@ -120,7 +120,8 @@ const NavBar: FC = () => {
                 router.push(`/Blogs/${selectedOption.value}`);
               } else if (selectedOption?.label.includes('Sponsors:')) {
                 router.push(`/Sponsors/${selectedOption.value}`);
-              }
+              } else if (selectedOption?.label.includes('Academic:')) {
+                router.push(`/Academics/${selectedOption.value}`);
               }
             }}
             options={options}
